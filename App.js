b0f89e8ef0ec442ed778f669e91ea41d72da25b5
JoaoMadeiraxyz <jaodisc@gmail.com>
2023-11-13 20:12:51 -0300

    finished the frontend inputs

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.label}>Insira qual a sua altura</Text>
+      <TextInput style={styles.input} placeholder="Altura" placeholderTextColor="white" />
+
+      <Text style={styles.label}>Insira qual o seu peso</Text>
+      <TextInput style={styles.input} placeholder="Peso" placeholderTextColor="white" />
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Calcular IMC</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'black',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  label: {
+    color: 'white',
+    marginBottom: 10,
+  },
+  input: {
+    backgroundColor: 'rgba(255, 255, 255, 0.2)',
+    width: '80%',
+    padding: 10,
+    color: 'white',
+    marginBottom: 24,
+  },
+  button: {
+    backgroundColor: 'blue', // Cor do botão, ajuste conforme necessário
+    padding: 15,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
   },
 });
+
+export default App;
