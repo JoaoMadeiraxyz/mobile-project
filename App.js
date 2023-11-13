efbecb509907d8653afeb4c9680ed54ec50d5468
JoaoMadeiraxyz <jaodisc@gmail.com>
2023-11-13 20:26:36 -0300

    finished the main page front end

--- App.js
+++ App.js
@@ -4,11 +4,17 @@ import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-nativ
 const App = () => {
   return (
     <View style={styles.container}>
+    <Text style={styles.label}>Insira qual o seu nome</Text>
+      <TextInput style={styles.input} placeholder="Nome" placeholderTextColor="white" />
+
       <Text style={styles.label}>Insira qual a sua altura</Text>
       <TextInput style={styles.input} placeholder="Altura" placeholderTextColor="white" />
 
       <Text style={styles.label}>Insira qual o seu peso</Text>
       <TextInput style={styles.input} placeholder="Peso" placeholderTextColor="white" />
+
+      <Text style={styles.label}>Insira qual a sua cidade</Text>
+      <TextInput style={styles.input} placeholder="Cidade" placeholderTextColor="white" />
 
       <TouchableOpacity style={styles.button}>
         <Text style={styles.buttonText}>Calcular IMC</Text>
@@ -20,12 +26,13 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
+    backgroundColor: '#1E1E1E',
     justifyContent: 'center',
     alignItems: 'center',
   },
   label: {
     color: 'white',
+    fontSize: 18,
     marginBottom: 10,
   },
   input: {
@@ -33,10 +40,12 @@ const styles = StyleSheet.create({
     width: '80%',
     padding: 10,
     color: 'white',
-    marginBottom: 24,
+    marginBottom: 36,
+    placeholderTextColor: "#CCCCCC",
+    borderRadius: 8,
   },
   button: {
-    backgroundColor: 'blue', // Cor do botão, ajuste conforme necessário
+    backgroundColor: '#6217b3',
     padding: 15,
     borderRadius: 10,
   },
