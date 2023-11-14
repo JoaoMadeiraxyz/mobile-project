8d3813c0828f715b80f15745cc4379f20ea60fe2
eduardo-possani <possaniduardo@gmail.com>
2023-11-13 22:05:24 -0300

    list rank

--- components/ranks.js
+++ components/ranks.js
@@ -1,28 +1,78 @@
-// components/rank.js
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+
+const data = [
+  { nome: 'João', cidade: 'São Paulo', imc: 24.5 },
+  { nome: 'Maria', cidade: 'Rio de Janeiro', imc: 22.0 },
+  { nome: 'Pedro', cidade: 'Belo Horizonte', imc: 26.8 },
+  // Adicione mais dados conforme necessário
+];
 
 const RankScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Detalhes</Text>
-      <Text>Esta é a página de detalhes</Text>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Nome</Text>
+        <Text style={styles.headerText}>Cidade</Text>
+        <Text style={styles.headerText}>IMC</Text>
+      </View>
+      <FlatList
+        style={styles.list}
+        data={data}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.item}>
+            <Text style={styles.itemText}>{item.nome}</Text>
+            <Text style={styles.itemText}>{item.cidade}</Text>
+            <Text style={styles.itemText}>{item.imc}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 2,
     backgroundColor: '#1E1E1E',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  list:{
+    width: '100%',
   },
   title: {
     color: 'white',
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 20,
+  },
+  header: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+  },
+  headerText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+    flex: 1,
+  },
+  item: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+    backgroundColor: 'rgba(255, 255, 255, 0.2)',
+    marginBottom: 5,
+    borderRadius: 8,
+  },
+  itemText: {
+    color: 'white',
+    fontSize: 16,
+    flex: 1,
   },
 });
 
